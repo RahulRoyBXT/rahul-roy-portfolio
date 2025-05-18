5de5e2624ca20717ab1eb28b0bd035c2a99580ce
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2025-05-19 00:54:37 +0530

    feat: Update .gitignore to include utils directory and comment out translation functions

--- src/utils/translationScript.ts
+++ src/utils/translationScript.ts
@@ -1,35 +1,35 @@
-import { enTranslations } from '../context/LanguageContext';
-import { translateObjectToJapanese, extractNewStringsForTranslation } from './translator';
+// import { enTranslations } from '../context/LanguageContext';
+// import { translateObjectToJapanese, extractNewStringsForTranslation } from './translator';
 
-export async function translateAll() {
-  console.log('Starting translation of all strings...');
-  const japaneseTranslations = await translateObjectToJapanese(enTranslations);
+// export async function translateAll() {
+//   console.log('Starting translation of all strings...');
+//   const japaneseTranslations = await translateObjectToJapanese(enTranslations);
   
-  console.log('Translation complete! Copy this into your LanguageContext:');
-  console.log(JSON.stringify({ ja: japaneseTranslations }, null, 2));
+//   console.log('Translation complete! Copy this into your LanguageContext:');
+//   console.log(JSON.stringify({ ja: japaneseTranslations }, null, 2));
   
-  return japaneseTranslations;
-}
+//   return japaneseTranslations;
+// }
 
-export async function translateNewStrings(currentJapaneseTranslations: Record<string, string>) {
+// export async function translateNewStrings(currentJapaneseTranslations: Record<string, string>) {
 
-  const newStrings = extractNewStringsForTranslation(enTranslations, currentJapaneseTranslations);
+//   const newStrings = extractNewStringsForTranslation(enTranslations, currentJapaneseTranslations);
   
-  console.log(`Found ${Object.keys(newStrings).length} new strings to translate`);
+//   console.log(`Found ${Object.keys(newStrings).length} new strings to translate`);
   
-  if (Object.keys(newStrings).length === 0) {
-    console.log('No new strings to translate!');
-    return currentJapaneseTranslations;
-  }
+//   if (Object.keys(newStrings).length === 0) {
+//     console.log('No new strings to translate!');
+//     return currentJapaneseTranslations;
+//   }
   
-  // Translate only the new strings
-  const newTranslations = await translateObjectToJapanese(newStrings);
+//   // Translate only the new strings
+//   const newTranslations = await translateObjectToJapanese(newStrings);
   
-  // Merge with existing translations
-  const updatedTranslations = {
-    ...currentJapaneseTranslations,
-    ...newTranslations
-  };
-;  
-  return updatedTranslations;
-}
+//   // Merge with existing translations
+//   const updatedTranslations = {
+//     ...currentJapaneseTranslations,
+//     ...newTranslations
+//   };
+// ;  
+//   return updatedTranslations;
+// }
